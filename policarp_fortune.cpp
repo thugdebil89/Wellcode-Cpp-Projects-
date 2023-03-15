67a13e7d7c7d95c65f16a792ff1102ead5a70418
Catalin Fistos <94680446+thugdebil89@users.noreply.github.com>
2023-03-15 21:24:55 +0100

    Polycarp's fortune
    Requirement
    Polycarp is very rich. He is so rich that all he knows about his wealth is that its value is a number consisting of a 1 followed by X of 0. Given the natural number X, display the value of Polycarp's wealth.
    
    Input data
    Number X is read.
    
    Output data
    The value of Polycarp's fortune is displayed.
    
    limitation
    X is a natural number less than 100.
    Example
    Input data
    4
    Output data
    10000

---
+++ policarp_fortune.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   long long int x;
+
+   cin >> x;
+
+   cout << 1;
+
+   while(x){
+
+      cout << 0;
+
+      x--;
+
+   }
+
+   return 0;
+
+}
